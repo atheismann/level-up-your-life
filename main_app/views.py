b7836fad50e8e70e71c5a2c4c41a970d3af5b908
Joseph Hunt <jhunted54@gmail.com>
2019-09-13 14:27:31 -0500

    updates gitignore

--- main_app/views.py
+++ main_app/views.py
@@ -9,7 +9,7 @@ from django.http import HttpResponse
 def home(request):
   return HttpResponse('<h1>Welcome to Level Up Your Life!</h1>')
 
-class JournalCreate(LoginRequiredMixin, CreateView):
+class JournalCreate(CreateView):
   model = Journal
   fields = ['title', 'author', 'about']
   def form_valid(self, form):
