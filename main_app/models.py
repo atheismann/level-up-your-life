994b64715a12f380cd863cf91463ce3cc6a6d617
Andrew Theismann <andrew@theismannconsulting.com>
2019-09-13 13:37:31 -0500

    Revert "extend user model"
    This reverts commit 406fb928df0f31a3c7f895bbe2e8cc209cd17f68.

--- main_app/models.py
+++ main_app/models.py
@@ -1,10 +1,3 @@
 from django.db import models
-from django.urls import reverse
-from datetime import date
-from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-class User(AbstractUser):
-  pass
-  score = models.IntegerField(default=0)
-  level = models.CharField(max_length=100, default='Newbie')
