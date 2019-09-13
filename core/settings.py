034025d8f6c7cbe4fbd256957e303ea8314738ef
Andrew Theismann <andrew@theismannconsulting.com>
2019-09-13 12:16:21 -0500

    connect heroku db

--- core/settings.py
+++ core/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {}
-DATABSES['default'] = dj_database_url.config(conn_max_age=600)
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
