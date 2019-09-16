ecedbc0af741f08ab6cf3c6e13df085a548c761a
Andrew Theismann <andrew@theismannconsulting.com>
2019-09-16 11:21:10 -0500

    update admin console

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Journal, Task, Post
 
 # Register your models here.
+admin.site.register(User, UserAdmin)
+admin.site.register(Journal)
+admin.site.register(Task)
+admin.site.register(Post)
--- main_app/urls.py
+++ main_app/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
   path('', views.home, name='home'),
-  path('journals/create/', views.journalsCreate.as_view(), name='journals_create'),
-  path('journals/', views.games_index, name='index'),
+  path('journals/create/', views.JournalCreate.as_view(), name='journals_create'),
+  path('journals/', views.journals_index, name='index'),
   path('journals/<int:journal_id>/', views.journals_detail, name='detail'),
   path('journals/<int:pk>/update/', views.JournalUpdate.as_view(), name='journals_update'),
   path('journals/<int:pk>/delete/', views.JournalDelete.as_view(), name='journals_delete'),
