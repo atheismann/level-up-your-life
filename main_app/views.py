c23e2c0e15027b5a8f351a763fad1ce2314d0d59
Joseph Hunt <jhunted54@gmail.com>
2019-09-16 09:51:00 -0500

    update templates

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic.edit import CreateView,
+from django.views.generic.edit import CreateView
 from .models import Journal
 
 # Add the following import
