542a678a16066582e2e53a330daaeedd6c2cf5dc
Joseph Hunt <jhunted54@gmail.com>
2019-09-16 11:45:33 -0500

    adds about route

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
   path('', views.home, name='home'),
+  path('about/', views.about, name='about'),
   path('journals/create/', views.JournalCreate.as_view(), name='journals_create'),
   path('journals/', views.journals_index, name='index'),
   path('journals/<int:journal_id>/', views.journals_detail, name='detail'),
