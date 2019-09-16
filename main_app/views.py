6652c5e2704f4c2b6b9b410e5fa1125209938ca1
Andrew Theismann <andrew@theismannconsulting.com>
2019-09-16 12:08:39 -0500

    add attachments

--- main_app/views.py
+++ main_app/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Journal
 from django.views.generic import DetailView
-
-# Add the following import
 from django.http import HttpResponse
 
+S3_BASE_URL = 'https://s3.us-east-2.amazonaws.com/'
+BUCKET = 'levelupyourlife-aplha'
 # Define the home view
 def home(request):
   return HttpResponse('<h1>Welcome to Level Up Your Life!</h1>')
@@ -37,3 +37,17 @@ def journals_detail(request, journal_id):
 
 def about(request):
   return render(request, 'about.html')
+
+def add_attachment(request, post_id):
+  photo_file = request.FILES.get('photo-file', None)
+  if photo_file:
+      s3 = boto3.client('s3')
+      key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
+      try:
+          s3.upload_fileobj(photo_file, BUCKET, key)
+          url = f"{S3_BASE_URL}{BUCKET}/{key}"
+          photo = Photo(url=url, post_id=post_id)
+          photo.save()
+      except:
+          print('An error occurred uploading file to S3')
+  return redirect('detail', post_id=post_id)
