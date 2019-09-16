f93818b2fc83f2cfc02ba8efe7bdb963db230f53
Joseph Hunt <jhunted54@gmail.com>
2019-09-16 11:21:09 -0500

    stash

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,9 +3,10 @@ from . import views
 
 urlpatterns = [
   path('', views.home, name='home'),
-  path('journals/create/', views.journalsCreate.as_view(), name='journals_create'),
-  path('journals/', views.games_index, name='index'),
+  path('journals/create/', views.JournalCreate.as_view(), name='journals_create'),
+  path('journals/', views.journals_index, name='index'),
   path('journals/<int:journal_id>/', views.journals_detail, name='detail'),
   path('journals/<int:pk>/update/', views.JournalUpdate.as_view(), name='journals_update'),
   path('journals/<int:pk>/delete/', views.JournalDelete.as_view(), name='journals_delete'),
+  path('journals/<int:journal_id>/assoc_post/<int:post_id>/', views.assoc_post, name='assoc_post'),
 ]
