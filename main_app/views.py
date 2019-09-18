20a7353e40d04a22177dc731705edcb0f6bbd1b4
Daniel <cermak222@gmail.com>
2019-09-18 10:18:55 -0500

    edit task detsils

--- main_app/views.py
+++ main_app/views.py
@@ -126,7 +126,7 @@ class EntryDelete(DeleteView):
 
 class TaskCreate(CreateView):
   model = Task
-  fields = ['title', 'description', 'importance', 'progress']
+  fields = ['title', 'description', 'importance']
 
   def form_valid(self, form):
     form.instance.user = self.request.user
